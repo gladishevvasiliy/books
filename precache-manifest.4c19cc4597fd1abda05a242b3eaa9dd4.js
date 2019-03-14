a5f8446d26bc3805719b2c6f3e1b03e2ad35186f
Vasiliy Gladishev <gladishevvasiliy@gmail.com>
2019-03-14 18:39:07 +0300

    Updates

---
+++ precache-manifest.4c19cc4597fd1abda05a242b3eaa9dd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "42fa08d959956c63e0d8",
+    "url": "/static/js/main.1b29d1a3.chunk.js"
+  },
+  {
+    "revision": "4ac68d88bdf411b2e030",
+    "url": "/static/js/2.4a9577b3.chunk.js"
+  },
+  {
+    "revision": "42fa08d959956c63e0d8",
+    "url": "/static/css/main.2d7fa86f.chunk.css"
+  },
+  {
+    "revision": "87faf4626df9dfb7d1e559f2a33cda90",
+    "url": "/index.html"
+  }
+];
