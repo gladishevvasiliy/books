864b00e8b422a6d8f2d56245cd8dbcf74ffcf365
vasiliy gladishev <gladishevvasiliy@gmail.com>
2019-04-09 23:20:31 +0300

    Updates

---
+++ precache-manifest.d02e61620242b71e47aace919bdd38b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f87e89041d8b8eae3928",
+    "url": "/books/static/js/runtime~main.82bd55ed.js"
+  },
+  {
+    "revision": "0ac0b9e703b2044c389c",
+    "url": "/books/static/js/main.928f45ff.chunk.js"
+  },
+  {
+    "revision": "94c1992658947e146f41",
+    "url": "/books/static/js/2.bc720744.chunk.js"
+  },
+  {
+    "revision": "0ac0b9e703b2044c389c",
+    "url": "/books/static/css/main.da6d9a27.chunk.css"
+  },
+  {
+    "revision": "14d61a55e27451f44e05dd73d21e79a6",
+    "url": "/books/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/books/precache-manifest.b1d699a72f60c41250717efd7a20c637.js"
+  "/books/precache-manifest.d02e61620242b71e47aace919bdd38b5.js"
 );
 
 workbox.clientsClaim();
